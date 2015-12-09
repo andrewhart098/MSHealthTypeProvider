1edb6156d415d7adaff72c2ac67d51e4e4244bac
Andrew Hart <andrewthart098@gmail.com>
2015-12-09 17:52:33 -0500

    Solution renamed again

--- MSHealth.Tests/ParsingTests.cs
+++ MSHealth.Tests/ParsingTests.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MSHealth.Tests
 {
     public class ParsingTests
     {
         public void CanParseActivities()
         {
-            
+            var activities = Device
         }
 
         public void CanParseDevices()
